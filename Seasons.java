2cbcaa06dea72dbbdf3a2e86786647b6192622a7
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-11 09:34:12 +0600

    Create Seasons.java

---
+++ Seasons.java
@@ -0,0 +1,25 @@
+public class Seasons {
+    public static String getSeasonByMonth(int monthNumber) {
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                return "Winter";
+            case 3:
+            case 4:
+            case 5:
+                return "Spring";
+            case 6:
+            case 7:
+            case 8:
+                return "Summer";
+            case 9:
+            case 10:
+            case 11:
+                return "Autumn";
+            default:
+                return "Invalid month number.";
+        }
+    }
+
+}
